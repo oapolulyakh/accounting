c5bd54dedc7a0e9a699842df3e747bbb2c956d8e
Oksana Polulyakh <o.a.polulyakh@gmail.com>
2026-01-08 23:50:42 +0300

    Remove dirty_main and clean requirments.txt

--- dirty_main.py
+++
@@ -1,8 +0,0 @@
-from application.db.people import *
-from application.salary import *
-from datetime import datetime
-
-if __name__ == '__main__':
-    print(f"Today is {datetime.date(datetime.now())} from {__name__}")
-    calculate_salary()
-    get_employees()
